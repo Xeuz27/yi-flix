8e8f442cd7772a54a43593e1cb684039d12aaaad
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2022-11-01 17:27:34 -0400

    created constants for outputting the errors to the client so he knows what is happening on registration

---
+++ includes/classes/Constants.php
@@ -0,0 +1,11 @@
+<?php
+class Constants {
+    public static $firstNameCharacters = "First name must have between 2 and 25 characters!";
+    public static $lastNameCharacters = "Last name must have between 2 and 25 characters!";
+    public static $usernameCharacters = "Username must have between 2 and 15 characters!";
+    public static $usernameTaken = "Username already exists!";
+    public static $emailsDontMatch = "Emails doesn't match!";
+    public static $emailNotValid = "Email is not valid!";
+    public static $emailTaken = "Email already exists!";
+}
+?>
