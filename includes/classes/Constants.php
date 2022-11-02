2ebe715e6b05edf015742cbc5bd7b8b3ddf34959
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2022-11-02 00:26:33 -0400

    added error constants for the passwords errors

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -4,8 +4,10 @@ class Constants {
     public static $lastNameCharacters = "Last name must have between 2 and 25 characters!";
     public static $usernameCharacters = "Username must have between 2 and 15 characters!";
     public static $usernameTaken = "Username already exists!";
-    public static $emailsDontMatch = "Emails doesn't match!";
+    public static $emailsDontMatch = "Emails don't match!";
     public static $emailNotValid = "Email is not valid!";
     public static $emailTaken = "Email already exists!";
+    public static $passwordsLength = "Password must have between 6 and 16 characters!";
+    public static $passwordsDontMatch = "Passwords don't match!";
 }
 ?>
