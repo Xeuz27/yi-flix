6094f127773f3c1e16df0c9d322fd0fdf0703185
Jesus <jesusg.1301.jg@gmail.com>
2022-11-06 02:38:10 -0400

    added jquery scripts and the script for the volumetoggle functoins

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,10 @@ $userLoggedIn = $_SESSION["userLoggedIn"];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Yi-flix | netflix clone for educational purposes</title>
-    <link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/d25b5653db.js" crossorigin="anonymous"></script>
+    <script src="assets/js/script.js"></script>
 </head>
 <body>
-    <div class="wrapper">
+    <div class='wrapper'>
