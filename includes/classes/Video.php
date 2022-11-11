a990cbe5e9921a97a166894edd901b90a435dfb5
Jesus <jesusg.1301.jg@gmail.com>
2022-11-11 18:09:24 -0400

    fixed query,was selecting wrong table

--- includes/classes/Video.php
+++ includes/classes/Video.php
@@ -4,15 +4,17 @@ class Video {
     public function __construct($con, $input) {
         $this->con = $con;
 
-        if (is_array($input)) {
+        if(is_array($input)) {
             $this->sqlData = $input;
-        } else {
-            $query = $this->con->prepare("SELECT * FROM entities WHERE id=:id");
+        }
+        else {
+            $query = $this->con->prepare("SELECT * FROM videos WHERE id=:id");
             $query->bindValue(":id", $input);
             $query->execute();
 
             $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
         }
+
         $this->entity = new Entity($con, $this->sqlData["entityId"]);
     }
     public function getId() {
