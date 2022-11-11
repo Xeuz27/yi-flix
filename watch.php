ef5b5ce9711914294bf44f01a00fc80dad9ca7fc
Jesus <jesusg.1301.jg@gmail.com>
2022-11-11 18:10:28 -0400

    created video player container, with go back button and controls

--- watch.php
+++ watch.php
@@ -1,11 +1,25 @@
 <?php
 require_once("includes/header.php");
-if(!isset($_GET["id"])){
+if (!isset($_GET["id"])) {
     ErrorMessage::show("No Id Found in url");
 }
 
 
 $video = new Video($con, $_GET["id"]);
-var_dump($video);
 $video->incrementViews();
 ?>
+<div class="watchContainer">
+    <div class="videoControls watchNav">
+        <button onclick="goBack()">
+            <i class="fa-solid fa-arrow-left-long"></i>
+        </button>
+        <h1>
+            <?php echo $video->getTitle(); ?>
+        </h1>
+
+    </div>
+    <video controls autoplay muted>
+        <source src="<?php echo $video->getFilePath(); ?>" type="video/mp4">
+    </video>
+</div>
+<script>initVideo();</script>
