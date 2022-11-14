61279ba0830b9fd71b68b8be11dde12bff6f38c6
Jesus <jesusg.1301.jg@gmail.com>
2022-11-14 16:42:30 -0400

    started creating upnext overlay to show next episode

--- watch.php
+++ watch.php
@@ -7,17 +7,22 @@ if (!isset($_GET["id"])) {
 
 $video = new Video($con, $_GET["id"]);
 $video->incrementViews();
+$upNextVideo = VideoProvider::getUpNext($con, $video);
 ?>
 <div class="watchContainer">
+
     <div class="videoControls watchNav">
-        <button onclick="goBack()">
-            <i class="fa-solid fa-arrow-left-long"></i>
-        </button>
+        <button onclick="goBack()"><i class="fa-solid fa-arrow-left-long"></i></button>
         <h1>
             <?php echo $video->getTitle(); ?>
         </h1>
-
     </div>
+    <div class="videoControls upNext">
+        <button><i class="fa-solid fa-redo"></i></button>
+        <h2>Up Next:</h2>
+        <h3><?php echo $upNextVideo->getTitle(); ?></h3>
+    </div>
+
     <video controls autoplay muted>
         <source src="<?php echo $video->getFilePath(); ?>" type="video/mp4">
     </video>
