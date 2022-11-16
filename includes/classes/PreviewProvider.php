a9102b7f06d472691ef923b9ae93f326bae6e693
Jesus <jesusg.1301.jg@gmail.com>
2022-11-16 19:40:23 -0400

    added funct to display continue watching or play text button, and subheading if it is movie or episode in season

--- includes/classes/PreviewProvider.php
+++ includes/classes/PreviewProvider.php
@@ -14,7 +14,13 @@ class PreviewProvider {
         $preview = $entity->getPreview();
         $thumbnail = $entity->getThumbnail();
 
-        //TODO: add a subtitle
+        $videoId = VideoProvider::getEntityVideoForUser($this->con, $id, $this->username);
+        $video = new Video($this->con, $videoId);
+        $inProgress = $video->isInProgress($this->username);
+        $playButtonText = $inProgress ? "Continue watching" : "Play";
+
+        $seasonEpisode = $video->getSeasonAndEpisode();
+        $subHeading = $video->isMovie() ? "" : "<h4>$seasonEpisode</h4>";
 
         return "<div class='previewContainer'>
                     <img src='$thumbnail' class='previewImage' hidden>
@@ -24,8 +30,9 @@ class PreviewProvider {
                     <div class='previewOverlay'>
                         <div class='mainDetails'>
                             <h3>$name</h3>
+                            $subHeading
                             <div class='buttons'>
-                                <button><i class='fa-solid fa-play'></i> Play</button>
+                                <button onclick='watchVideo($videoId)'><i class='fa-solid fa-play'></i> $playButtonText</button>
                                 <button onclick='volumeToggle(this)'><i class='fa-solid fa-volume-xmark'></i></button>
                             </div>
                         </div>
