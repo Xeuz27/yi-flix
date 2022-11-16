034a7a28d495d0f4ffac7f78ef7094593c43201a
Jesus <jesusg.1301.jg@gmail.com>
2022-11-16 02:43:41 -0400

    created query function to get next entity video for user based on what he's seen

--- includes/classes/videoProvider.php
+++ includes/classes/videoProvider.php
@@ -27,6 +27,30 @@ class VideoProvider {
         $row = $query->fetch(PDO::FETCH_ASSOC);
         return  new Video ($con,$row);
     }
+    public static function getEntityVideoForUser ($con, $username, $entityId) {
+        $query = $con->prepare("SELECT videoId from videoProgress
+                                INNER JOIN videos
+                                ON videoProgress.videoId = videos.id
+                                WHERE videos.entityId =:entityId
+                                AND videoProgress.username =:username
+                                ORDER BY videoProgress.dateModified DESC
+                                LIMIT 1");
+        $query->bindValue(":entityId", $entityId);
+        $query->bindValue(":username", $username);
+        $query->execute();
 
+        if($query->rowCount() == 0){
+            $query = $con->prepare("SELECT id FROM videos
+                                    WHERE entityId =:entityId
+                                    ORDER BY season, episode ASC
+                                    LIMIT 1");
+            $query->bindValue(":entityId", $entityId);
+            $query->execute();
+
+        }
+        return $query->fetchColumn();
+
+                                
+    }
 }
 ?>
