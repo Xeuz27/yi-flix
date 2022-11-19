b225be6f420b739d4077a7ea9cf68c6e0f292643
Jesus <jesusg.1301.jg@gmail.com>
2022-11-19 17:57:45 -0400

    added functions to select only tv shows or movies categories and display them on page

--- includes/classes/CategoryContainers.php
+++ includes/classes/CategoryContainers.php
@@ -17,6 +17,32 @@ class CategoryContainers {
         return $html . "</div>";
 
     }
+    public function showTvShowCategories (){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+        $html = "<div class='previewCategories'>
+                    <h1>TV Shows</h1>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $html .= $this->getCategoryHtml($row, null, true, false);
+        }
+
+        return $html . "</div>";
+
+    }
+    public function showMovieCategories (){
+        $query = $this->con->prepare("SELECT * FROM categories");
+        $query->execute();
+        $html = "<div class='previewCategories'>
+                    <h1>Movies</h1>";
+
+        while($row = $query->fetch(PDO::FETCH_ASSOC)){
+            $html .= $this->getCategoryHtml($row, null, false, true);
+        }
+
+        return $html . "</div>";
+
+    }
 
     public function showCategory($categoryId, $title = null) {
         $query = $this->con->prepare("SELECT * FROM categories WHERE id=:id");
@@ -40,12 +66,13 @@ class CategoryContainers {
         }
         
         else if($tvShows) {
-            //get tv show entities
+            $entities = EntityProvider::getTvShowEntities($this->con, $categoryId, 30);
+
 
         }
         
         else {
-            //get movie entities
+            $entities = EntityProvider::getMoviesEntities($this->con, $categoryId, 30);
 
         }
         if(sizeof($entities) == 0 ){
@@ -58,7 +85,7 @@ class CategoryContainers {
             $entitiesHtml .= $previewProvider->createEntityPreviewSquare($entity);
         }
         return "<div class='category'>
-                    <a href='cayegory.php?id:$categoryId'>
+                    <a href='category.php?id=$categoryId'>
                         <h3>$title</h3>
                     </a>
                     <div    class='entities'>
