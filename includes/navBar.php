fee56e255cd9357826fd188d833b55f28e7409c0
Jesus <jesusg.1301.jg@gmail.com>
2022-11-19 16:48:42 -0400

    created navbar, added styles and method to hide it, and fade it in and out

---
+++ includes/navBar.php
@@ -0,0 +1,22 @@
+<div class="topBar">
+    <div class="logoContainer">
+        <a href="index.php">
+            <img src="./assets/img/yiflix-logo.png" alt="yiflix logo">
+        </a>
+    </div>
+
+    <ul class="navLinks">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="shows.php">Tv Shows</a></li>
+        <li><a href="movies.php">Movies</a></li>
+    </ul>
+
+    <div class="rightItems">
+        <a href="seach.php">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </a>
+        <a href="profile.php">
+            <i class="fa-solid fa-user"></i>
+        </a>
+    </div>
+</div>
