94d40b1e77fab9d22267283b245daab55bafe4a0
Jesus <jesusg.1301.jg@gmail.com>
2022-12-07 11:23:50 -0400

    added functions to validate and update password

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -114,7 +114,50 @@ class Account {
         $this->validateFirstName($fn);
         $this->validateLastName($ln);
         $this->validateNewEmail($ln, $un);
+
+        if(empty($this->errorArray)){
+            $query = $this->con->prepare("UPDATE users
+                                        SET firsName:fn, lastName:ln,
+                                            email:em, username=:un");
+
+            $query->bindValue(":fn", $fn);
+            $query->bindValue(":ln", $fn);
+            $query->bindValue(":em", $em);
+            $query->bindValue(":un", $un);
+
+            return $query->execute();
+        }
+        return false;
     }
+    public function updatePassword($oldPw, $pw, $pw2, $un){
+        $this->validateOldPassword($oldPw, $un);
+        $this->validatePasswords($pw, $pw2);
+    
+        if(empty($this->errorArray)){
+            $query = $this->con->prepare("UPDATE users
+                                        SET password=:pw, username=:un");
+            $pw = hash("sha512", $pw);
+            $query->bindValue(":pw", $pw);
+            $query->bindValue(":un", $un);
+
+            return $query->execute();
+        }
+        return false;
+    }
+    public function validateOldPassword($oldPw, $un){
+        $pw = hash("sha512", $oldPw);
+
+        $query = $this->con->prepare("SELECT * FROM users WHERE username=:un AND password=:pw");
+        $query->bindValue(":un", $un);
+        $query->bindValue(":pw", $pw);
+
+        $query->execute();
+
+        if($query->rowCount() ==0 ){
+            array_push($this->errorArray, Constants::$passwordIncorrect);
+        }
+    }
+
     private function validateNewEmail($em, $un){
 
         if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
@@ -137,6 +180,12 @@ class Account {
         }
     }
 
+    public function getFirstError (){
+        if(!empty($this->errorArray)){
+            return $this->errorArray[0];
+        }
+
+    }
 
 }
 ?>
